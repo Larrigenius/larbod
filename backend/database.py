2de4fb9430a306ddedc4be7cd513fdfff8340d6e
Ayobode Omobosoye <borowax007@gmail.com>
2026-09-23 06:35:50 -0700

    Configure database connection pooling

--- backend/database.py
+++ backend/database.py
@@ -14,7 +14,14 @@ DATABASE_URL = URL.create(
 )
 
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True,
+)
 
 
 SessionLocal = sessionmaker(
